e3f8c41ed64b5bafa0c94a931b5b8e9e3a9af3e9
Sondinh <dnson20022001@gmail.com>
2021-08-31 23:30:07 +0700

    add Chapter8

---
+++ Chapter8/counter/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { number: 1 };
+    }
+
+    add() {
+        this.setState({ number: this.state.number + 1 });
+    }
+    minus() {
+        this.setState({ number: this.state.number - 1 });
+    }
+
+    render() {
+        return (
+            <div>
+                Tính năng tăng giảm số
+                <h3>{this.state.number}</h3>
+                <button onClick={() => this.add()}>Tăng</button>
+                <button onClick={() => this.minus()}>Giảm</button>
+            </div>
+        );
+    }
+}
+
+export default App;
