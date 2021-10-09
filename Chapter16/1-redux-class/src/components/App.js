4731005fb2711ef209255fee77f28bcba63dc954
Sondinh <dnson20022001@gmail.com>
2021-10-09 22:26:51 +0700

    add Chapter16

---
+++ Chapter16/1-redux-class/src/components/App.js
@@ -0,0 +1,14 @@
+import "../App.css";
+import PostList from "./PostList";
+import PostListHook from "./PostListHook";
+
+function App() {
+    return (
+        <div className="ui">
+            <PostList></PostList>
+            {/* <PostListHook /> */}
+        </div>
+    );
+}
+
+export default App;
