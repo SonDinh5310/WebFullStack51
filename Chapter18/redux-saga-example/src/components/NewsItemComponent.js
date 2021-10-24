250cffe703077a2deb7a6b77a57ae2acbccec56c
Sondinh <dnson20022001@gmail.com>
2021-10-24 17:33:39 +0700

    add Chapter18

---
+++ Chapter18/redux-saga-example/src/components/NewsItemComponent.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const articlesStyle = {
+    color: "olive",
+    border: "1px solid",
+};
+
+class NewsItemComponent extends React.Component {
+    render() {
+        const articles = this.props.articles;
+        console.log("debug-NewsItemComponent: ", articles);
+        return articles && articles.length > 0
+            ? articles.map((article, index) => {
+                  return (
+                      <div style={articlesStyle} key={index}>
+                          <h2>{article.title}</h2>
+                          <img
+                              style={{ width: "200px", height: "150px" }}
+                              src={article.urlToImage}
+                              alt=""
+                          />
+                          <h3>{article.description}</h3>
+                          <h4>
+                              {new Date(
+                                  article.publishedAt
+                              ).toLocaleDateString()}
+                          </h4>
+                          <a href={article.url}>Read more</a>
+                      </div>
+                  );
+              })
+            : null;
+    }
+}
+
+const mapStateToProps = (state) => ({
+    articles: state.news,
+});
+
+export default connect(mapStateToProps, null)(NewsItemComponent);
